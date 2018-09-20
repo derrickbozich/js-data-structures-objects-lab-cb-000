259b89a94b2deff83976dabbe074099897cd86ff
derrick b <dbozich1@gmail.com>
2018-09-20 04:32:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,9 @@ function updateDriverWithKeyAndValue(driver, key, value) {
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
   return Object.assign(driver, {[key]: value});
 }
+
+function deleteFromDriverByKey(driver, key) {
+  const newObj = { ...driver };
+
+  return delete newObj[key]
+}
